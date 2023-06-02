7609b8995151e9de0f9bbc85f8d0896cba49b786
Caio531 <132024149+Caio531@users.noreply.github.com>
2023-06-02 19:43:00 -0300

    Add files via upload

---
+++ lista9/l2q1.py
@@ -0,0 +1,12 @@
+
+pessoa = {
+    'primeiro_nome': 'DUDU',
+    'sobrenome': 'AZEVEDO',
+    'idade': 30,
+    'cidade': 'Pernanbuco'
+}
+
+print("Primeiro nome:", pessoa['primeiro_nome'])
+print("Sobrenome:", pessoa['sobrenome'])
+print("Idade:", pessoa['idade'])
+print("Cidade:", pessoa['cidade'])
